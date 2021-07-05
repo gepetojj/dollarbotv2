6000d56703c90f5537604cbc2413cb3683834ce8
Gepetojj <nmjoaopedro22@gmail.com>
2021-07-05 16:42:05 -0300

    Correção de falhas

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -21,25 +20,3 @@ const config = {
 };
 
 export default config;
-=======
-import dotenv from "dotenv";
-dotenv.config();
-
-const config = {
-	dev: Boolean(process.env.NODE_ENV === "development" ? true : false),
-	token: String(process.env.TOKEN),
-
-	pkid: String(process.env.PKID),
-	pk: String(process.env.PK).replace(/\\n/g, "\n"),
-	ce: String(process.env.CE),
-	cid: String(process.env.CID),
-	curl: String(process.env.CURL),
-
-	richPresence: {
-		options: ["monopoly.", "dólares fora.", "jogo do bixo.", "tempo fora."],
-		timeout: 7000,
-	},
-};
-
-export default config;
->>>>>>> d55c329de5feeeaaabae95e4cab8cf5216f33c9b
