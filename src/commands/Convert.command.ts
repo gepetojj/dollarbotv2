39d8d248ca2442243541ad100c86f203403c7c5d
Gepetojj <nmjoaopedro22@gmail.com>
2021-07-18 20:23:50 -0300

    Novo comando 'convert'

---
+++ src/commands/Convert.command.ts
@@ -0,0 +1,100 @@
+import Discord from "discord.js";
+
+import { IBotCommand } from "./IBotCommand";
+import { CommandErrorsEmbed } from "../templates";
+import { AwesomeAPIProvider } from "../providers";
+import config from "../config";
+
+export default class ConvertCommand implements IBotCommand {
+	name: string;
+	syntax: string;
+	arguments: boolean;
+	guildOnly: boolean;
+	description: string;
+	aliases?: string[];
+	cooldown?: number;
+
+	constructor() {
+		this.name = "convert";
+		this.syntax = "convert {moeda} {valor}";
+		this.arguments = true;
+		this.description = "Converte o valor para a moeda escolhida.";
+		this.aliases = ["converter"];
+	}
+
+	async command(message: Discord.Message, args: string[]) {
+		message.channel.startTyping();
+
+		const coin = args[0];
+		const value = args[1];
+
+		const BRL = 0;
+		const USD = 1;
+		const validSyntaxBRL = ["brl", "real", "rs"];
+		const validSyntaxUSD = ["usd", "dol", "dolar", "dollar"];
+
+		if (!coin || !value) {
+			const errorEmbed = new CommandErrorsEmbed()
+				.generate(message)
+				.addField("Causa do erro:", "Moeda ou valor inválido.")
+				.addField("Moedas aceitas:", "brl (Real) | usd (Dólar)");
+			await message.channel.send(errorEmbed);
+			message.channel.stopTyping(true);
+			return;
+		}
+
+		if (Number(value) === NaN) {
+			const errorEmbed = new CommandErrorsEmbed()
+				.generate(message)
+				.addField(
+					"Causa do erro:",
+					"Valor inválido. Digite um número."
+				);
+			await message.channel.send(errorEmbed);
+			message.channel.stopTyping(true);
+			return;
+		}
+
+		const convertTo = validSyntaxBRL.includes(coin.toLowerCase())
+			? BRL
+			: validSyntaxUSD.includes(coin.toLowerCase())
+			? USD
+			: null;
+
+		if (convertTo === null) {
+			const errorEmbed = new CommandErrorsEmbed()
+				.generate(message)
+				.addField("Causa do erro:", "Moeda inválida.")
+				.addField("Moedas aceitas:", "brl (Real) | usd (Dólar)");
+			await message.channel.send(errorEmbed);
+			message.channel.stopTyping(true);
+			return;
+		}
+
+		const provider = new AwesomeAPIProvider();
+		const lastDollarValue = await provider.getLastDollarValue();
+
+		const operation =
+			convertTo === BRL
+				? Number(value) / lastDollarValue.value
+				: Number(value) * lastDollarValue.value;
+		const finalValue = operation.toFixed(2);
+
+		const embed = new Discord.MessageEmbed()
+			.setColor(config.primaryColor)
+			.setTitle("Conversão de valores")
+			.setDescription(
+				`${value} ${
+					convertTo === BRL ? "dólares" : "reais"
+				} atualmente valem ${finalValue} ${
+					convertTo === BRL ? "reais" : "dólares"
+				}.`
+			)
+			.setFooter(
+				`Comando executado por: ${message.author.tag}`,
+				message.author.avatarURL()
+			);
+		await message.reply(embed);
+		message.channel.stopTyping();
+	}
+}
--- src/loaders/CommandsLoader.ts
+++ src/loaders/CommandsLoader.ts
@@ -1,21 +1,29 @@
+import fs from "fs";
 import path from "path";
 
-import commands from "../commands";
+import config from "../config";
 import { IBotCommand } from "../commands/IBotCommand";
-
 export class CommandsLoader {
 	commandsPath: string; // Caminho para a pasta de comandos
 	commandsList: string[]; // Lista de nomes dos comandos
 	commandsMap: IBotCommand[]; // Lista de objetos dos comandos
 
 	constructor() {
-		let list: string[] = [];
-		commands.forEach((command) => {
-			list.push(command.name);
-		});
 		this.commandsPath = path.resolve(__dirname, "..", "commands");
-		this.commandsList = list;
-		this.commandsMap = commands;
+		this.commandsList = fs
+			.readdirSync(this.commandsPath)
+			.filter((file) =>
+				file.endsWith(config.dev ? ".command.ts" : ".command.js")
+			);
+		this.commandsMap = [];
+
+		for (const commandName of this.commandsList) {
+			const commandPath = path.resolve(this.commandsPath, commandName);
+			const Command = require(commandPath);
+
+			const command = new Command.default();
+			this.commandsMap.push(command);
+		}
 	}
 
 	getCommands() {
